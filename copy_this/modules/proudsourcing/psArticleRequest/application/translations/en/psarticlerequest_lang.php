f016ce16e40b9b7afa24c6545a8a84467d334a73
Heinrich Franz <heinrichfranz@hf-laptop.fritz.box>
2017-04-26 20:50:18 +0200

    added english translation

---
+++ copy_this/modules/proudsourcing/psArticleRequest/application/translations/en/psarticlerequest_lang.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @copyright (c) Proud Sourcing GmbH | 2014
+ * @link www.proudcommerce.com
+ * @package psArticleRequest
+ * @version 1.0.0
+ **/
+$sLangName  = "English";
+
+$aLang = array(
+		'charset'                                   => 'UTF-8',
+
+                'NAVIGATION_PSMODULE'                           => 'Proud Sourcing Module',
+                'EXCEPTION_INPUT_NOVALIDEMAIL'                  => "Invalid email address",
+                'PS_ARTICLEREQUEST_TAB_LINK_TITLE'          => "Availability request",
+                'PS_ARTICLEREQUEST_WHEN_INFORM_TITLE'           => "Gladly we notify you when this product is available again. Simply enter your email address.",
+                'PS_ARTICLEREQUEST_EMAIL'                                       => "email address",
+                'PS_ARTICLEREQUEST_VERIFICATIONCODE'            => "Security code",
+                'PS_ARTICLEREQUEST_SEND'                                        => "Submit",
+                'PS_ARTICLEREQUEST_SEND_SUBJECT'                        => "Request",
+                'PS_ARTICLEREQUEST_SEND_SUBJECT_AV'             => "Back in stock: ",
+                'PS_ARTICLEREQUEST_SUCCESS'                                     => "<font color='green'><b>We have received your request.</b></font>",
+
+        'PS_ARTICLE_REQUEST_MAIL1'                  => "Thank you for your interest in our products.",
+        'PS_ARTICLE_REQUEST_MAIL2'                  => "As soon as the product",
+        'PS_ARTICLE_REQUEST_MAIL3'                  => "is in stock again, we will notify you.",
+                'EXCEPTION_INPUT_NOTALLFIELDS'                          => "Specify a value for!",
+
+);
